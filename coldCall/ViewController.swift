37e7cafd31d1abc0de55f13f9a14e839c4023d19
Liseth Cardozo <cardozoliseth@gmail.com>
2017-09-06 18:47:22 -0700

    Initial Commit

--- coldCall/ViewController.swift
+++ coldCall/ViewController.swift
@@ -10,8 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    let names = ["Ali", "Anna", "Jhon", "Alex"]
+    
+    @IBAction func callCallButton(_ sender: UIButton) {
+        let index = Int(arc4random_uniform(4))
+        displayLabel.text = names[index]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+      
         // Do any additional setup after loading the view, typically from a nib.
     }
 
