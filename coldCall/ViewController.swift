d3a073d31c87c97e49bf9186fabdad7dc272ae4e
Liseth Cardozo <cardozoliseth@gmail.com>
2017-09-06 20:39:57 -0700

    Add random number display

--- coldCall/ViewController.swift
+++ coldCall/ViewController.swift
@@ -12,11 +12,25 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var displayLabel: UILabel!
     
-    let names = ["Ali", "Anna", "Jhon", "Alex"]
+    @IBOutlet weak var numberLabel: UILabel!
     
+    
+    let names = ["Ali", "Anna", "Jhon", "Alex"]
+    let numbers = ["1", "2", "3", "4", "5"]
     @IBAction func callCallButton(_ sender: UIButton) {
         let index = Int(arc4random_uniform(4))
+        let indexNum = Int(arc4random_uniform(5))
         displayLabel.text = names[index]
+        numberLabel.text = numbers[indexNum]
+        if numberLabel.text == "1" || numberLabel.text == "2" {
+            numberLabel.textColor = UIColor.red
+        }
+        else if numberLabel.text == "3" || numberLabel.text == "4" {
+            numberLabel.textColor = UIColor.orange
+        } else {
+            numberLabel.textColor = UIColor.green
+        }
+        
     }
     
     override func viewDidLoad() {
